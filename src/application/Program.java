c7cb2f6620789ce222100ae1f3e215c2f1f25f06
Yuri Baldan <yuribbp@gmail.com>
2020-09-22 18:00:08 -0300

    Listando folders

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,26 @@
 package application;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS: ");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		sc.close();
 	}
 
 }
